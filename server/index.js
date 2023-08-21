7bd383a66b938b25d80ec33c7bbe0e3c4e8b1d14
Carlos Eduardo <carloseduardo.na7@gmail.com>
2023-08-21 17:18:14 -0300

    att

--- server/index.js
+++ server/index.js
@@ -1,30 +1,32 @@
 const express = require("express");
 const app = express();
-const mysql = require("mysql");
+const mysql = require("mysql2");
+const cors = require("cors");
 
-const db = mysql.createPool({
+const db = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "senha1234",
     database: "banco",
 })
 
-app.get('/', (rep, res) => {
-    db.query("INSERT INTO usuarios (email, password) VALUES('carlos@gmail.com', '12345678')"
-    );
-})
+app.use(express.json())
+app.use(cors())
 
-// app.get('/', async (req, res) => {
-//     try {
-//         const result = await db.query("INSERT INT usuarios (email, password) VALUES ('carlos@gmail.com', '12345678')");
-//         console.log("Registro inserido com sucesso :", result);
-//         res.status(200).send("Registro inserido com sucesso");
-//     } catch (error) {
-//         console.error("Erro ao inserir registro:", error);
-//         res.status(500).send("Erro ao inserir registro");
-//     }
-// });
+app.post("/register", (req, res) => {
+    const email = req.body.email;
+
+    db.query(
+        "SELECT * FROM usuarios WHERE email=?", [email], (err, result) => {
+        if(err) {
+            res.send(err)
+            return
+        }
+        res.send(result);
+    })
+})
 
 app.listen(3001, () => {
     console.log("rodando na porta 3001")
 })
+
