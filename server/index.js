213ca5e949cf8b6e222c2ab1b1a12f8efd1cd652
Carlos Eduardo <carloseduardo.na7@gmail.com>
2023-08-22 15:04:24 -0300

    att

--- server/index.js
+++ server/index.js
@@ -15,42 +15,6 @@ const db = mysql.createConnection({
 app.use(express.json())
 app.use(cors())
 
-// app.post("/login", (req, res) => {
-// 	const email = req.body.email;
-// 	const password = req.body.password;
-
-// 	db.query(
-// 		"SELECT * FROM usuarios WHERE email = ?", [email], (err, result) => {
-
-			// if(err) {
-			// 	// res.send(err) exibir o erro "err" está dando problema, abaixo é melhor
-			// 	res.status(500).json({ error: "Erro no servidor" })
-			// 	return;
-			// }
-
-// 			if(result.length > 0) {
-// 				const storedHash = result[0].password;
-
-// 				bcrypt.compare(password, storedHash, (erro, response) => {
-
-// 					if (erro) {
-// 						res.send(erro);
-// 					}
-// 					if(response) {
-// 						res.send(response)
-// 						// res.send({msg: "Usuario logado!"})
-// 					} else {
-// 						res.send(response)
-// 						// res.send({msg: "Senha incorreta!"})
-// 					}
-// 				})
-// 			} else {
-// 				res.send({msg: "Conta não encontrada!"})
-// 			}
-// 		}
-// 	)
-// })
-
 app.post("/login", (req, res) => {
 	const email = req.body.email;
 	const password = req.body.password;
@@ -58,16 +22,18 @@ app.post("/login", (req, res) => {
 	db.query(
 		"SELECT * FROM usuarios WHERE email = ?", [email], (err, result) => {
 			if (err) {
-				res.status(500).json({ error: "Erro no servidor" })
+				// res.send(err) // exibir o erro "err" está dando problema, abaixo é melhor
+				res.status(500).json({ msg: "Erro no servidor" })
 				return;
 			}
 
 			if (result.length > 0) {
-				const hashedPassword = result[0].password; // Obtém a senha hashada do banco de dados
+				const hashPassword = result[0].password;
 
-				bcrypt.compare(password, hashedPassword, (err, response) => {
-					if (err) {
-						res.status(500).json({ error: "Erro no servidor" })
+				bcrypt.compare(password, hashPassword, (erro, response) => {
+					if (erro) {
+						// res.send(erro) // exibir o erro "err" está dando problema, abaixo é melhor
+						res.status(500).json({ msg: "Erro no servidor" })
 						return;
 					}
 
@@ -76,6 +42,7 @@ app.post("/login", (req, res) => {
 						// res.send(result) // causa erro no nodemon
 						res.json({ msg: "Login bem-sucedido" })
 					} else {
+						res.send(response) // false
 						res.status(401).json({ msg: "Email ou Senha incorretos!" })
 					}
 				})
@@ -92,7 +59,6 @@ app.post("/register", (req, res) => {
 
 	db.query(
 		"SELECT * FROM usuarios WHERE email=?", [email], (err, result) => {
-
 			if(err) {
 				res.send(err)
 				return;
@@ -102,7 +68,6 @@ app.post("/register", (req, res) => {
 			bcrypt.hash(password, saltRounds, (err, hash) => {
 				db.query(
 					"INSERT INTO usuarios (email, password) VALUES (?, ?)", [email, hash], (erro, result) => {
-						
 						if(erro) {
 							res.send(erro)
 							return;
@@ -121,4 +86,3 @@ app.post("/register", (req, res) => {
 app.listen(3001, () => {
 	console.log("rodando na porta 3001")
 })
-
