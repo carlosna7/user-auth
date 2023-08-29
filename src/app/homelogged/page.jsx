c38cf05ad80c2845c04fe834311597e9a7dafad1
Carlos Eduardo <carloseduardo.na7@gmail.com>
2023-08-29 09:35:17 -0300

    att

--- src/app/homelogged/page.jsx
+++ src/app/homelogged/page.jsx
@@ -1,49 +1,8 @@
 'use client'
 
-import jwt from "jsonwebtoken";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
-
 const Homelogged = () => {
 
-  const router = useRouter()
-
-  const getCookie = (name) => {
-    const value = `; ${document.cookie}`;
-    const parts = value.split(`; ${name}=`);
-    if (parts.length === 2) {
-      return parts.pop().split(";").shift();
-    }
-  };
-
-  const isAuthenticated = () => {
-    const token = getCookie("token");
-
-    console.log(token)
   
-    if (!token) {
-      console.log(token)
-      return false;
-    }
-  
-    try {
-      // Verify the JWT token on the client-side
-      const decodedToken = jwt.verify(token, "secret-key-secret");
-      console.log(decodedToken)
-      return true;
-    } catch (error) {
-      return false;
-    }
-  };
-
-  useEffect(() => {
-    if (!isAuthenticated()) {
-      router.push("/login");
-      console.log("não autenticado")
-    } else {
-      console.log("autenticado")
-    }
-  }, []);
 
   return (
     <div>
