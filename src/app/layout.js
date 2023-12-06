635620ac53c7ffee4520d80ac042645c0141d5d3
Carlos Eduardo <carloseduardo.na7@gmail.com>
2023-12-06 11:00:35 -0300

    Apply context

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import { AuthProvider } from '@/context/authContext'
 import './globals.css'
 import { Inter } from 'next/font/google'
 
@@ -10,8 +11,12 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="pt-br">
-      <body className={inter.className}>{children}</body>
-    </html>
+
+    <AuthProvider>
+      <html lang="pt-br">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </AuthProvider>
+    
   )
 }
